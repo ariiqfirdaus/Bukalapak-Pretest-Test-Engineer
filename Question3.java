cc836f823e7b0f3c9016d84a0150c4f11d90e529
Muhammad Ariiq Firdaus <68710956+ariiqfirdaus@users.noreply.github.com>
2020-07-24 11:23:39 +0700

    Push from github upload.

---
+++ Question3.java
@@ -0,0 +1,9 @@
+public class Question3 {
+    public static void main(String[] args) {
+        String name, address;
+
+        name = "Jack Donovan";
+        address = "3566 Golden Street Miami";
+        System.out.println(address.replace(" ", ""));
+    }
+}
